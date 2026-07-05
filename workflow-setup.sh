b94b4b51916aeca38d9dd559dd617d2df9bee274
Mariano Benitez <mariano.benitez@farmacity.com.ar>
2026-07-05 18:02:51 -0300

    - usamos una acción específica para acelerar la instalación de latex

--- workflow-setup.sh
+++ workflow-setup.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo apt-get update
-sudo apt-get install -y \
-  latexmk \
-  texlive-xetex \
-  texlive-latex-recommended \
-  texlive-latex-extra \
-  texlive-fonts-recommended \
-  texlive-pictures
+echo "Workflow setup complete."
